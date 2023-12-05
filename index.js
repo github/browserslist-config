8209536d8ea86fa25251f2e46a9761529b3abbf3
HBS <30705623+MrHBS@users.noreply.github.com>
2023-12-05 11:58:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 module.exports = [
   'last 10 Chrome versions',
+  'last 10 ChromeAndroid versions',
   'last 10 Edge versions',
   'last 10 Firefox versions',
   'last 3 Safari major versions',
+  'last 3 iOS major versions',
   'Firefox ESR'
 ]
